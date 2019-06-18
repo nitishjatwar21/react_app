0742461a2746f320ec3fa9d644e1adca7c375172
Nitish Jatwar <nkjatwar@gmail.com>
2019-06-18 08:14:07 -0700

    Updates

---
+++ precache-manifest.56b709a4500ba674f0e12a2135f72721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfd162cf024413752d8a5d15e5357a5",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "67a7f03a5e0ef9c57266",
+    "url": "/react_app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "998dc72191b46501fc4e",
+    "url": "/react_app/static/css/main.f10c67d5.chunk.css"
+  },
+  {
+    "revision": "67a7f03a5e0ef9c57266",
+    "url": "/react_app/static/js/2.817aa922.chunk.js"
+  },
+  {
+    "revision": "998dc72191b46501fc4e",
+    "url": "/react_app/static/js/main.094e6b07.chunk.js"
+  },
+  {
+    "revision": "dd7a70cdeda87e112e79",
+    "url": "/react_app/static/js/runtime~main.cba007e3.js"
+  }
+]);
